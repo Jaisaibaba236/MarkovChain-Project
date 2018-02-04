aa57c5830193613915531c8fb65124daa83326c7
Jaisaibaba236 <jay.patel76@dal.ca>
2018-02-04 13:14:14 -0400

    Moving forward to implement HTML page

--- views.py
+++ views.py
@@ -1,62 +1,62 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 import sqlite3
+from flask import Flask, render_template, url_for, request
+app = Flask(__name__)
 
 # Create your views here
 class HomePageView(TemplateView):
    def get(self, request, **kwargs):
       return render(request, 'index.html', context=None)
 
-
 class Database():
 
-     def __init__(self):
-        self.word_dictionary = dict()
-        self.conn = sqlite3.connect('/home/jaisaibaba236/Desktop/markov_project/MarkovChain.db')
-        print(type(self.conn))
+   def __init__(self):
+      self.word_dictionary = dict()
+      self.conn = sqlite3.connect('/home/jaisaibaba236/Desktop/markov_project/MarkovChain.db')
+      self.cursor = self.conn.cursor()
         
-     def db_connection(self):
-#        connection = sqlite3.connect(db_file)
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Markov'")
-        existance = cursor.fetchone()
-        if existance:
-#            cursor.execute("INSERT INTO Markov VALUES(1,'Audi',52642)")
-             print('Table exists.')
-        else:
-             cursor.execute("CREATE TABLE MARKOV(Word TEXT, Following_Word TEXT)")
-             print('Table created successfully.')
-        
-#       cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Markov'")
-#       rows = cur.fetchone()
-#       for row in rows:
-#            print (row)
-#       self.conn.close()
- 
-     def word_matchcase(self,user_input):
-        sentence_words = user_input.split()
+   def table_creation(self):
+      self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='MARKOV'")
+      existance = self.cursor.fetchone()  # existance is a tuple  
+      if existance:
+         print('Table exists.')
+      else:
+         cursor.execute("CREATE TABLE MARKOV(Word TEXT, Following_Word TEXT)")
+         print('Table created successfully.')
 
-        for word in range(len(sentence_words)-1):
-            cursor.execute('SELECT Word FROM Markov WHERE Word ='+str(sentence_words[word]))
-            word_list = cursor.fetchone()
+   def word_matchcase(self,user_input):
+      sentence_words = user_input.split()
 
-            if sentence_words[word] not in word_list:
-                 cursor.execute('INSERT INTO Markov (Word) VALUES('+str(sentence_words[word])
-    cursor.execute('INSERT INTO Markov (Following_Word) VALUES('+str(sentence_words[word+1])+') WHERE Word =str(sentence_words[word])')
-#                 self.conn[sentence_words[word]] = sentence_words[word+1]
-            else:
-                 if self.conn[sentence_words[word]] == sentence_words[word+1]:
-                     print(self.conn[sentence_words[word]])
-                 else:
-                     self.conn[sentence_words[word]] = sentence_words[word+1]
-        print(self.conn)
+      for word in range(len(sentence_words)-1):
+         word_list = self.cursor.execute("SELECT WORD FROM MARKOV").fetchall()
+         if sentence_words[word] not in word_list:
+            self.cursor.execute("INSERT INTO MARKOV VALUES ('{0}','{1}');".format(sentence_words[word],sentence_words[word+1]))
+            self.conn.commit()
+         else: 
+            following_word = self.cursor.execute("SELECT FOLLOWING_WORD FROM MARKOV WHERE WORD = '{0}'".format(sentence_words[word])).fetchall()
+            print(following_word)
 
-#if __name__ == '__main__':
-#    db_connection('/home/jaisaibaba236/Desktop/markov_project/MarkovChain.db')      
-  
+   @app.route('/http://127.0.0.1:8000/',methods=['POST']) 
+   def database_update(self):
+     with app.test_request_context():
+       start_word = request.form.get('starting_word')
+       print(start_word)
+
+       for word in range(len(start_word)-1):
+         word_list = self.cursor.execute("SELECT WORD FROM MARKOV").fetchall()
+         if start_word[word] not in word_list:
+            self.cursor.execute("INSERT INTO MARKOV VALUES ('{0}','{1}');".format(start_word[word],start_word[word+1]))
+            self.conn.commit()
+         else: 
+            following_word = self.cursor.execute("SELECT FOLLOWING_WORD FROM MARKOV WHERE WORD = '{0}'".format(start_word[word])).fetchone()
+            print(following_word)
+
+       return render_template('database_update.html', form=form)
 
 database_init = Database() 
-database_init.db_connection()
+database_init.table_creation()
+database_init.database_update()
 
-database_init.word_matchcase('this is a python tutorial and this is a very interesting development language as compared to other development languages')
+#'this is a python tutorial and this is a very interesting development language as compared to other development languages')
 
